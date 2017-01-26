620b751bd18ab267c936a5debdd312676a733c51
Arianit Hetemi <Arianit Hetemi>
2017-01-26 18:44:17 +0100

    Added JS validation with Parsley

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,23 +2,31 @@
 
 @section('title', '| Create New Post')
 
+@section('stylesheets')
+	<link rel="stylesheet" type="text/css" href="{{ elixir('css/parsley.css') }}">
+@stop
+
 @section('content')
 	<div class="row">
 		<div class="col-md-8 col-md-offset-2">
 	    	<h1>Create New Post</h1>
 	    	<hr>
-	    	<form method="POST" action="{{ route('posts.store') }}">
+	    	<form data-parsley-validate method="POST" action="{{ route('posts.store') }}">
 		    	{{ csrf_field() }}
 		      	<div class="form-group">
 		        	<label name="title" >Title:</label>
-		        	<input id="title" name="title" class="form-control">
+		        	<input id="title" name="title" class="form-control" required max-length="255">
 		      	</div>
 		      	<div class="form-group">
 		        	<label name="body">Post Body:</label>
-		        	<textarea id="body" name="body" rows="10" class="form-control"></textarea>
+		        	<textarea id="body" name="body" rows="10" class="form-control" required></textarea>
 		      	</div>
 		      	<input type="submit" value="Create Post" class="btn btn-success btn-lg btn-block">
 	    	</form>
 	  	</div>
 	</div>﻿
+@stop
+
+@section('scripts')
+	<script src="{{ elixir('js/parsley.min.js') }}"></script>
 @stop
