56c6ee6222da836fa88fcd6ca509eb9853bb3877
Arianit Hetemi <Arianit Hetemi>
2017-02-02 23:05:12 +0100

    Changed UI/UX to v0.3

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,7 +4,33 @@
 
 @section('content')
 	<div class="container">
-		<h1>{{ $post->title }}</h1>
-		<p>{{ $post->body }}</p>
+		<div class="row show-post-container">
+			<div class="col-md-8">
+				<h1>{{ $post->title }}</h1>
+				<p class="text-justify">{{ $post->body }}</p>
+			</div>
+			
+			<div class="col-md-4 sidebar-for-post">
+				<div class="well">
+					<dl class="dl-horizontal">
+						<dt>Created At:</dt>
+						<dd>{{ date('M j, Y h:ia', strtotime($post->created_at)) }}</dd>
+					</dl>
+					<dl class="dl-horizontal">
+						<dt>Last Updated:</dt>
+						<dd>{{ date('M j, Y h:ia', strtotime($post->updated_at)) }}</dd>
+					</dl>
+					<hr />
+					<div class="row">
+						<div class="col-md-6">
+							<a href="{{ route('posts.edit', $post->id) }}" class="btn btn-primary btn-block">Edit</a>
+						</div>
+						<div class="col-md-6">
+							<a href="{{ route('posts.destroy', $post->id) }}" class="btn btn-danger btn-block">Delete</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 @stop
